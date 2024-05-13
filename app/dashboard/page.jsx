a6283143b38416dc720d8279375ffa75a4e41468
Atul Tingre <atultingre.work@gmail.com>
2024-05-14 01:46:31 +0530

    Dark Mode Theme Added

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useTheme } from "next-themes";
 
 const dashboard = () => {
+  const { setTheme } = useTheme();
+
+  useEffect(()=>{
+    setTheme('light')
+  },[])
   return <div>dashboard</div>;
 };
 
