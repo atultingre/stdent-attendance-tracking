6d0c6eeb673b9dae3b81d72fd16274953f148f2b
Atul Tingre <atultingre.work@gmail.com>
2024-05-14 03:03:12 +0530

    sidenav active route addded

--- app/dashboard/_components/SideNav.jsx
+++ app/dashboard/_components/SideNav.jsx
@@ -1,19 +1,15 @@
 "use client";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import {
-  GraduationCap,
-  Hand,
-  LayoutDashboard,
-  LayoutIcon,
-  Settings,
-} from "lucide-react";
+import { GraduationCap, Hand, LayoutDashboard, Settings } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 const SideNav = () => {
+  const router = useRouter();
+  const pathname = usePathname();
   const { user } = useKindeBrowserClient();
-  console.log("user: ", user);
   const menuList = [
     {
       id: 1,
@@ -53,7 +49,13 @@ const SideNav = () => {
 
       {menuList.map((menu) => (
         <Link href={menu?.path} className="" key={menu?.id}>
-          <h2 className="flex items-center gap-3 text-md p-4 text-slate-500 hover:bg-primary hover:text-white hover:rounded-lg cursor-pointer my-2">
+          <h2
+            className={`flex items-center gap-3 text-md p-4 text-slate-500 ${
+              pathname === menu.path
+                ? "bg-primary text-white rounded-lg cursor-pointer my-2"
+                : "hover:bg-primary hover:text-white hover:rounded-lg cursor-pointer my-2"
+            } `}
+          >
             <menu.icon />
             {menu?.name}
           </h2>
